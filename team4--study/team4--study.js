3b2fb19e042d4c454ba1fbcfddbd1d6ff49bb984
vaio <orangeeeee.ore.ore@gmail.com>
2016-01-02 16:44:07 +0900

    例文コミット

--- team4--study/team4--study.js
+++ team4--study/team4--study.js
@@ -1,44 +1,33 @@
-var Todo = Backbone.Model.extend({
-
-	/*---------------------------------------
-	 ユーザが指定したデータが不完全だった場合などに備えて、
-	 モデルに対してデフォルトの属性値をセットしておきたい場合に使用する。
-	*/
-	default: {
-		title: '',
-		completed: false
-	}
-	//値が正しい場合、何もreturnしてはならない。
-	validate: function (attribs) {
-		if (attribs.title === undefined) {
-			return "タイトルを付けてください。";
-		}
-	},
-
-	// Modelのインスタンスが作成された際に呼び出される処理（作成は任意）
-	initialize: function () {
-		console.log('モデルが初期化されました。');
-
-		//Modelの変化の漢詩
-		this.on('change', function () {
-			console.log('Model value has been changed.')
-		});
-		/*
-		  titleのみ変更された場合は、以下のようにイベント監視する。
-		　on('change:title')
-		*/
-		//
-		this.on("invalid")
-
-	},
-});
-
 $(function () {
 
-	$('#add-btn').click(function () {
-		var myTodo = new Todo();
-		myTodo.set('title', '属性値が変化するとLisnerが呼び出される。');
-		console.log('タイトルが変更されました' + myTodo.get('title'));
+	//test1ボタンクリックイベント
+	$('#test1-btn').on('click', function () {
 
+		//----------------------------------------
+		// 参考演算子やIf文を使用するよりsimpleで良い。
+		//----------------------------------------
+		console.log(testOrFunc1());
+		console.log(testOrFunc1('引数で渡した!'));
+
+		console.log('---------------');
+		console.log(testAndFunc1());
+		console.log(testAndFunc1('xxx'));
 	});
 });
+
+testOrFunc1 = function (param) {
+
+	var _param = param || 'unknown';
+	//参考演算子を使用した場合
+	//var _param = (param === undefined) ? 'unknown' : param;
+
+	return 'param is ' + _param;
+
+};
+testAndFunc1 = function (param) {
+
+	var _param = param && 'unknown';
+
+	return 'param is ' + _param;
+
+};
