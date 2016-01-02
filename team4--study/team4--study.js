b571ee5e90c18b4dd431c3aadb69e969e255e8e8
vaio <orangeeeee.ore.ore@gmail.com>
2016-01-02 18:13:02 +0900

    例文を修正

--- team4--study/team4--study.js
+++ team4--study/team4--study.js
@@ -19,7 +19,7 @@ testOrFunc1 = function (param) {
 
 	var _param = param || 'unknown';
 	//参考演算子を使用した場合
-	//var _param = (param === undefined) ? 'unknown' : param;
+	//var _param = param ? param : 'unknown';
 
 	return 'param is ' + _param;
 
@@ -31,3 +31,16 @@ testAndFunc1 = function (param) {
 	return 'param is ' + _param;
 
 };
+
+//falsyとtruthyについて
+/*
+'truty', 'falsy' のそもそもの意味は、 Boolean の 'true', 'false' ではないんだけど、評価すると 'true', 'false' と振る舞うものです。(つまり 'false' も含みます)
+論理演算等で暗黙的に変換されたときの値ですね。そこから 'true values', 'false values' とも呼ばれるようです。
+JavaScript であれば、 'falsy' な値として以下のものがあります。
+*/
+console.log(false ? 'truty' : 'falsy')
+console.log(undefined ? 'truty' : 'falsy')
+console.log(null ? 'truty' : 'falsy')
+console.log(0 ? 'truty' : 'falsy')
+console.log(NaN ? 'truty' : 'falsy')
+console.log('' ? 'truty' : 'falsy')
