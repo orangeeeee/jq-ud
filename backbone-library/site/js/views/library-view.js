beeda6d5b7b32090c1ed49ba3254cd739bf3ae41
vaio <orangeeeee.ore.ore@gmail.com>
2016-01-03 17:03:06 +0900

    通信なしで、さらでモデルの追加

--- backbone-library/site/js/views/library-view.js
+++ backbone-library/site/js/views/library-view.js
@@ -1,13 +1,40 @@
 app = app || {};
 
 app.LibraryView = Backbone.View.extend({
+	//div
 	el: '#books',
 
 	initialize: function (initialBooks) {
 		this.collection = new app.Library(initialBooks);
 		this.render();
+		//新しくmodelが追加された場合に起こるイベント
+		//'click #add': 'addBook'で「追加」ボタンのイベントが起こり
+		//モデルをcollectionにadd後に走る。
+		this.listenTo( this.collection, 'add', this.renderBook );
 	},
 
+	events: {
+		'click #add': 'addBook'
+	},
+
+	addBook: function (e) {
+		//イベントがキャンセル可能である場合、上位ノードへのイベントの
+		//伝播 (propagation) を止めずに、そのイベントをキャンセルします。
+		//再描画を避けるためにイベントをキャンセルする。
+		e.preventDefault;
+
+		var formData = {};
+
+		$('#addBook div').children('input').each(function (i, el) {
+
+			if ($(el).val() != '') {
+				console.log('el.id:' + el.id);
+				formData[el.id]	 = $(el).val();
+			}
+		});
+		this.collection.add(new app.Book(formData));
+	},
+	
 	// render library by rendering each book in its collection
 	render: function () {
 		this.collection.each(function (item) {
@@ -22,6 +49,8 @@ app.LibraryView = Backbone.View.extend({
 		var bookView = new app.BookView({
 			model: item
 		});
+		//id="books"の一番後ろに挿入。
 		this.$el.append(bookView.render().el);
 	}
+	
 });
