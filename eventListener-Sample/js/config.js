6fdd9391a29988b4c9ec4563b9dcd044f6255e27
air mac <orangeeeee.ore.ore@gmail.com>
2017-03-11 21:49:52 +0900

    extends

---
+++ eventListener-Sample/js/config.js
@@ -0,0 +1,10 @@
+var defalut_dialog_option = {
+    title: "いけざき",
+    modal: true, // モーダル表示する
+    resizable: false, // リサイズしない
+    draggable: false, // ドラッグしない
+    show: "clip", // 表示時のエフェクト
+    hide: "fade", // 非表示時のエフェクト
+    width: "auto",
+    height: "auto"
+}
--- eventListener-Sample/js/test.js
+++ eventListener-Sample/js/test.js
@@ -1,10 +1,10 @@
-$(function () {
+$(function() {
 
     var template_table_row = _.template($('#template-rowHTML').html());
     // jquery - ui - dialog用
     var template_dialog = _.template($('#template-ui-dialog').html());
 
-    $("#addHTML").on("click", function () {
+    $("#addHTML").on("click", function() {
         //追加行のデータを取得
         let addData = getRowData();
         //Tableに行を追加
@@ -12,35 +12,34 @@ $(function () {
     });
 
 
-    $("#openDialog").on("click", function () {
+    $("#openDialog").on("click", function() {
 
-        /* 必要なときに、必要なdiv要素を生成して、利用する */
-        var ui_template = $("<div></div>").dialog({
+        //dialogオブジェクト生成
+        var dialogObj = $("<div></div>").dialog({
             autoOpen: false
         });
-        /* ダイアログの中身は、どこかから取ってきたり、べた書きしたり */
-        ui_template.html(template_dialog(getData()));
-        /* ダイアログのオプションを設定して */
-        ui_template.dialog("option", {
-            title: "いけざき",
-            width: 600,
-            height: "auto",
+        // contents部分をtemplateを使用し追加
+        dialogObj.html(template_dialog);
+        //optionを拡張
+        var dialog_option = $.extend(defalut_dialog_option, {
             buttons: {
-                "Close": function () {
+                "Close": function() {
                     $(this).dialog("close");
                 }
             }
         });
-        /* ダイアログを開きます */
-        ui_template.dialog("open");
+        //optionを追加
+        dialogObj.dialog("option", dialog_option);
+        // dialogを開く
+        dialogObj.dialog("open");
     });
 
     //例１
-    $("img-click").on("click", function () {
+    $("img-click").on("click", function() {
         alert("ss");
     });
 
-    $("body").on("click", ".img-click", function () {
+    $("body").on("click", ".img-click", function() {
         console.log("click image div");
         alert("No Yeah");
     });
